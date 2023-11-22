8b87f6d00c5f2d4b6d4e45c25b38f3946ad5b59b
LucasdeLima1 <drlucaslima1@gmail.com>
2023-11-22 20:13:45 -0300

    Atualizando curso de JavaScript

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do carro é ${vel} Km/h.`)
+if (vel > 60) { /* Condição simples*/
+    console.log(`Você ultrapassou a velocidade. MULTADO!`)
+}
+console.log('Dirija usando cinto de segurança!')
