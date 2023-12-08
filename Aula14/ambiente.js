df9ce0057dbb7a17c5d67d695c6d5d94f815c950
LucasdeLima1 <drlucaslima1@gmail.com>
2023-12-08 12:20:54 -0300

    Atualizando curso de JavaScript

---
+++ Aula14/ambiente.js
@@ -0,0 +1,17 @@
+console.log(`Vai começar...`)
+
+for (c=1;c<=4;c++) {
+    console.log(`Passo ${c}`)
+}
+
+console.log(`Fim`)
+
+
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
