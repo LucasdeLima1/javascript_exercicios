9fbe09ba349df7772d2b2546609aa72a32d59dd5
LucasdeLima1 <drlucaslima1@gmail.com>
2023-12-11 23:07:59 -0300

    Atualizando exercícios

--- Aula 14ex/ex016/javascript.js
+++ Aula 14ex/ex016/javascript.js
@@ -1,11 +1,12 @@
-/* function contar() {
+function contar() {
     var res = document.getElementById('res')
     var n1 = document.getElementById('ini')
     var n2 = document.getElementById('fim')
     res.innerHTML = `Oi, estou testando... ${n1, n2}`
 }
-*/
 
+
+/*
 var n1 = 1
 var n2 = 4
 var c = 1
@@ -14,3 +15,5 @@ while (c < 4) {
     c = n1
     c = n1 + 2
 } 
+
+*/
